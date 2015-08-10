f3c3d593e5debb0f457be52a721f4f57890c0cb1
root <root@localhost.localdomain>
2015-08-09 17:11:45 -0700

    Add one more line to the main program
    Signed-off-by Faugeras <faugeras.sun@gmail.com>

--- source/zhou-yi.c
+++ source/zhou-yi.c
@@ -2,6 +2,7 @@
 int main(void)
 {
  printf("Zhou-Yi is a program to understand the relationship of  unvierse and human beings/n");
+ printf("We are going to use computer and big data technologies to present a picture of how universe and human being interact each other!/n");
  return 0;
 }
 
